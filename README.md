3661a906299dcb3817feb1d20fa249bf5ba2a60a
Marcin Trendota <marcin@trendota.com>
2022-09-18 22:14:57 +0200

    Translate descriptions

--- README.md
+++ README.md
@@ -12,3 +12,14 @@ Translates
 
 You may use `translate_all` script to translate everything (in module's directory only). Put relevant files into `origs`
 subdirectory and run script with module's path as parameter. Script depends on `python3` existence.
+
+As for now only Polish language is supported. Multi-language support will eventually be implemented.
+# Important note
+
+I will not add `*_desc` files as it probably violates license. You have to prepare these files by yourself an put it
+into `lang/pl/` directory. Their format is as follows:
+`original name=description`, one item per line (like ordinary INI file).
+
+For example:
+
+`Athletics=<p>Description</p>`
--- translate.py
+++ translate.py
@@ -6,6 +6,7 @@ from utils import read_names
 
 def main(file_name, translate_type):
     names = read_names(os.path.join("lang/pl", translate_type))
+    desc = read_names(f"lang/pl/{translate_type}_desc")
     with open(file_name) as plik:
         for linia in plik.readlines():
             a = json.loads(linia)
@@ -13,8 +14,13 @@ def main(file_name, translate_type):
                 translated_name = names[a["name"]]
             except KeyError:
                 translated_name = a["name"]
+            try:
+                translated_desc = desc[a["name"]]
+            except KeyError:
+                translated_desc = a["data"]["description"]["value"]
             a["name"] = translated_name
-            print(json.dumps(a))
+            a["data"]["description"]["value"] = translated_desc
+            print(json.dumps(a, ensure_ascii=False))
 
 
 if __name__ == "__main__":
