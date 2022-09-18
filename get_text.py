ee573c6dfaa58138ee8baaf27ba3b671d67bfc88
Marcin Trendota <marcin@trendota.com>
2022-09-18 22:09:51 +0200

    Possibility to get original descriptions

---
+++ get_text.py
@@ -0,0 +1,20 @@
+import sys
+import json
+
+
+def main(file_name, data_type):
+    with open(file_name) as plik:
+        for linia in plik.readlines():
+            a = json.loads(linia)
+            if data_type == "names":
+                print(a["name"])
+            if data_type == "descriptions":
+                print(f'{a["name"]}={json.dumps(a["data"]["description"]["value"], ensure_ascii=False)[1:-1]}')
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        data = "names"
+        if len(sys.argv) == 3:
+            data = sys.argv[2]
+        main(sys.argv[1], data)
